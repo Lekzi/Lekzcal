bd026abaceb6bd4a113a73a9a07daa442b3e0a0d
Lekzi <93331411+Lekzi@users.noreply.github.com>
2022-01-12 07:27:10 +0100

    Update Lekzcal.py

--- Lekzcal.py
+++ Lekzcal.py
@@ -1,3 +1,24 @@
+import time as t
+import os
+t.sleep(5)
+x= "Enter a valid option!"
+os.system ("clear")
+print ("""Lekzcal is a python programme for performing simple math operation""")
+t.sleep(8)
+os.system("clear")
+t.sleep(4)
+def loop():
+	os.system ("clear")
+	os.system (" figlet Lekzcal")
+	t.sleep(6)
+	print("""
+	Tool Name:Lekzcal
+	Creator:Lêkzï
+	Contact me on whatsapp:+2347064822519
+	Team:Greyhaxors 
+	""")
+	print("""Special thanks to Spider Anongrey hat for his support""")
+loop()
 operation = input('''
 Please type in the math operation you would like to complete:
 + for addition
